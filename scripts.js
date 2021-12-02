8b1e6473b8bb111205b3b224556993eb6c8c1313
Raquel <raquelbreternitz@gmail.com>
2021-12-01 18:17:51 -0600

    button toggle working

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//TOGGLE THEME
+
+let toggle = document.querySelector('.toggle-theme');
+
+toggle.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if (document.body.classList.contains('dark-mode')) {
+    // Turning the theme off:
+    document.body.classList.remove('dark-mode');
+    // Reverse logic on the button text, so that users can turn
+    // the theme back on:
+    //toggle.innerText = 'Turn theme on';
+  } else {
+    document.body.classList.add('dark-mode');
+    //toggle.innerText = 'Turn theme off';
+  }
+});
