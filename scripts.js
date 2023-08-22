3ad5874d6b676ddc373070c3cc44c627512f1293
Raquel Breternitz <raquelbreternitz@gmail.com>
2023-08-22 19:57:47 +0100

    added aria-label switcher, working on focus state

--- scripts.js
+++ scripts.js
@@ -10,14 +10,18 @@ toggle.addEventListener("click", function (e) {
   if (document.body.classList.contains("dark-mode")) {
     // Turning the theme off:
     document.body.classList.remove("dark-mode");
+
     // Reverse logic on the button text, so that users can turn
     // the theme back on:
     //toggle.innerText = 'lights off';
     document.getElementById("curve-text").textContent = "lights off";
+    toggle.ariaLabel = "dark mode off";
   } else {
     document.body.classList.add("dark-mode");
     //toggle.innerText = 'lights on';
     document.getElementById("curve-text").textContent = "lights on";
+    toggle.ariaLabel = "dark mode on";
+
   }
 });
 
